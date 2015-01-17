caa8cea3bda3b9d6a8f15de797f8abf25619823b
jude <jude@affle.com>
2015-01-17 18:34:09 +0800

    initial commit

---
+++ while-loops/src/whileLoops.java
@@ -0,0 +1,15 @@
+
+public class whileLoops
+    {
+    public static void main(String[] args)
+        {
+        int value = 0;
+
+        while (value < 10)
+            {
+            System.out.println("Hello " + value);
+            //this is equal to value = value +1;
+            value += 1;
+            }
+        }
+    }
